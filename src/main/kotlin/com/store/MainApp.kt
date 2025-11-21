fb0e708946e3fbc805167e782ff15637f23d9ebd
Saachi Kaup <saachi.kaup@gmail.com>
2025-11-21 16:20:01 +0530

    Refactored MainApp.kt to not use earlier verbose config code

--- src/main/kotlin/com/store/MainApp.kt
+++ src/main/kotlin/com/store/MainApp.kt
@@ -2,35 +2,9 @@ package com.store
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Primary
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.DeserializationFeature
 
 @SpringBootApplication
-open class Application {
-
-    @Bean
-    @Primary
-    open fun objectMapper(builder: Jackson2ObjectMapperBuilder): ObjectMapper {
-        return builder.build<ObjectMapper>().apply {
-            // Disable all type coercion - reject when wrong types are sent
-            coercionConfigDefaults().setCoercion(
-                com.fasterxml.jackson.databind.cfg.CoercionInputShape.Integer,
-                com.fasterxml.jackson.databind.cfg.CoercionAction.Fail
-            )
-            coercionConfigDefaults().setCoercion(
-                com.fasterxml.jackson.databind.cfg.CoercionInputShape.Float,
-                com.fasterxml.jackson.databind.cfg.CoercionAction.Fail
-            )
-            coercionConfigDefaults().setCoercion(
-                com.fasterxml.jackson.databind.cfg.CoercionInputShape.Boolean,
-                com.fasterxml.jackson.databind.cfg.CoercionAction.Fail
-            )
-        }
-    }
-}
+open class Application
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
